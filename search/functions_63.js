5d1c751f2d9d348e0fa0323b4bd08d1e65b69b2c
Kevin Han <thehyphenator9@gmail.com>
2012-05-07 15:04:34 -0500

    ...

--- search/functions_63.js
+++ search/functions_63.js
@@ -4,10 +4,10 @@ var searchData=
   ['clamppt',['clampPt',['../namespacecvutils_1_1geom.html#ab040ea663623480d3fa5fa29b63eefe2',1,'cvutils::geom']]],
   ['click',['click',['../namespaceosx_1_1mouse.html#a127098ef29a5e3b824fdc2376e89f557',1,'osx::mouse']]],
   ['closest',['closest',['../structkmath_1_1_interval.html#af6b38235f08105530f09dbdc2a4cb10c',1,'kmath::Interval']]],
-  ['cluster',['cluster',['../namespacecvutils.html#acbbf9e8cbd6decade9a79781d981d085',1,'cvutils']]],
+  ['cluster',['cluster',['../namespaceseq.html#aa94b667465072146a3badd193f77d2be',1,'seq']]],
   ['combinations',['combinations',['../namespacekmath.html#a53519d2ec30d9df038dbe8ceaead1a9c',1,'kmath']]],
   ['contains',['contains',['../structkmath_1_1_interval.html#af196d10186744c7f3adc855e7343bb1e',1,'kmath::Interval']]],
   ['countelems',['countElems',['../namespacedict.html#a0a95e9cde2e3b78db4cbacd7ec00934f',1,'dict']]],
   ['cursorfinder',['CursorFinder',['../classhumancv_1_1_cursor_finder.html#a3ff8b8961e65f437484cc61bfb28dee1',1,'humancv::CursorFinder']]],
-  ['cvtbgr2rg',['cvtBGR2RG',['../namespacecvutils.html#afce5aa1e29332624c3f7cbabcbe19f69',1,'cvutils']]]
+  ['cvtbgr2rg',['cvtBGR2RG',['../namespacecvutils.html#a3b7156ed31820f4f6064135921984f11',1,'cvutils']]]
 ];
--- search/related_6f.js
+++ search/related_6f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c',['operator<<',['../structhumancv_1_1_finger_data.html#a6341d1d30d8b360b043084bd59b601ea',1,'humancv::FingerData::operator&lt;&lt;()'],['../structkmath_1_1_point.html#ac6ce84350ccb7814a8ebbc3b93fa0fcf',1,'kmath::Point::operator&lt;&lt;()'],['../structmouse_1_1_state.html#a8c73b18ef2e5fc88aa180424b78bba09',1,'mouse::State::operator&lt;&lt;()']]]
+  ['operator_3c_3c',['operator<<',['../structautodiff_1_1_dual_num.html#a7ffe2d1d115a2c8145f8a9b5613d103b',1,'autodiff::DualNum::operator&lt;&lt;()'],['../structhumancv_1_1_finger_data.html#a6341d1d30d8b360b043084bd59b601ea',1,'humancv::FingerData::operator&lt;&lt;()'],['../structkmath_1_1_point.html#ac6ce84350ccb7814a8ebbc3b93fa0fcf',1,'kmath::Point::operator&lt;&lt;()'],['../structmouse_1_1_state.html#a8c73b18ef2e5fc88aa180424b78bba09',1,'mouse::State::operator&lt;&lt;()'],['../structseq_1_1_elem_view.html#a83157ceaa5e29e3d24a5c46292584a8d',1,'seq::ElemView::operator&lt;&lt;()'],['../structseq_1_1_wrapped_seq.html#ac87322cd928f8f98e06d379189d5902b',1,'seq::WrappedSeq::operator&lt;&lt;()']]]
 ];
